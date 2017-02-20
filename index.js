91c6f249fb9626b7c3a35c6767d0fc4742ab2982
ebere10 <ebere10@hotmail.com>
2017-02-20 13:26:43 +0000

    filled in app.js

--- index.js
+++ index.js
@@ -1,18 +1,11 @@
 const express = require('express');
-const expressJWT = require('express-jwt');
+// const expressJWT = require('express-jwt');
 const port    = process.env.PORT || 3000;
 const app     = express();
 const dest    = `${__dirname}/public`;
 
 app.use(express.static(dest));
 
-app.use('/api', expressJWT({ secret: secret })
-  .unless({
-    path: [
-      { url: '/api/login', methods: ['POST'] },
-      { url: '/api/register', methods: ['POST'] }
-    ]
-  }));
 
 app.get('/*', (req, res) => res.sendFile(`${dest}/index.html`));
 
