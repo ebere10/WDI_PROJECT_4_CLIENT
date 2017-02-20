ba7a5ddd5467cdcf34d7b380a077abc221d4bb9c
ebere10 <ebere10@hotmail.com>
2017-02-20 12:34:23 +0000

    corrected the url for user register in user.factory.js

--- src/js/factories/user.factory.js
+++ src/js/factories/user.factory.js
@@ -4,7 +4,7 @@ angular
 
 userFactory.$inject = ['API', '$resource'];
 function userFactory(API, $resource){
-  return $resource(`${API}/users/:id`, { id: '@_id'}, {
-    'register': { method: 'POST', url: `$http://localhost:3000/register` }
+  return $resource('$http://localhost:3000/users/:id', { id: '@_id'}, {
+    'register': { method: 'POST', url: '$http://localhost:3000/register' }
   });
 }
