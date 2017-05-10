11eba66a5a2aeeda5102a6701476d75cbe5266c4
ebere10 <ebere10@hotmail.com>
2017-05-10 15:01:36 +0100

    changed API address, removed forward slash

--- src/js/constants/api.constant.js
+++ src/js/constants/api.constant.js
@@ -1,5 +1,5 @@
 angular
   .module('mindPops')
-  .constant('API', 'https://post-something-api.herokuapp.com/');
+  .constant('API', 'https://post-something-api.herokuapp.com');
   // .constant('API', 'http://localhost:3000');
   // .constant('API', `${window.location.origin}/api`);
